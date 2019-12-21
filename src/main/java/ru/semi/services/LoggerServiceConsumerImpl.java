9e7facdf71e621d4ef1b201fdbe11ff7e982169d
Sulaymon <demirel6777@gmail.com>
2019-12-21 09:01:27 +0300

    fixed queue name

--- src/main/java/ru/semi/services/LoggerServiceConsumerImpl.java
+++ src/main/java/ru/semi/services/LoggerServiceConsumerImpl.java
@@ -5,20 +5,22 @@ import org.springframework.stereotype.Service;
 import ru.semi.dto.LogInfo;
 import ru.semi.services.interfaces.LoggerServiceConsumer;
 
+import static ru.semi.config.RabbitMQ.*;
+
 @Service
 public class LoggerServiceConsumerImpl implements LoggerServiceConsumer {
 
-    @RabbitListener(queues = "log-queue")
+    @RabbitListener(queues = LOG_QUEUE)
     public void receiveLogData(String message) {
         // TODO: 30.11.2019 save into cassandra
     }
 
-    @RabbitListener(queues = "error-log-queue")
+    @RabbitListener(queues = ERROR_LOG_QUEUE)
     public void receiveErrorLogData(String message) {
         // TODO: 30.11.2019 save into elastic by index log-error
     }
 
-    @RabbitListener(queues = "business-log-queue")
+    @RabbitListener(queues = BUSINESS_LOGIC_LOG_QUEUE)
     public void receiveBusinessLogData(String message) {
         // TODO: 30.11.2019 save into elastic by index business-logic
     }
